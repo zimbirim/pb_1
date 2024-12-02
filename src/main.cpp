5921d41698c6d3751d7e002eead233738dfe98d0
Евгений Козлов <j.kozlov2006ayandex.ru@gmail.com>
2024-12-02 10:16:03 +0300

    вариант поля 1

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,49 @@
 ///
 void KarelProgram() {
 /// @todo Добавить программу для Карела
+TurnLeft();
+while(FrontIsClear() == true){
+    TurnLeft();
+    TurnLeft();
+    TurnLeft();
+    while(FrontIsClear() == true){
+        if(BeepersPresent() == true){
+            PickBeeper();
+        }
+        if(BeepersPresent() == false and FrontIsClear() == true){
+            Move();
+        }
+    }
+    while (BeepersPresent() == true){
+        PickBeeper();
+    }
+    TurnLeft();
+    Move();
+    TurnLeft();
+    while(FrontIsClear() == true){
+        Move();
+    }
+    TurnLeft();
+    TurnLeft();
+    TurnLeft();
+}
+TurnLeft();
+TurnLeft();
+TurnLeft();
+while(FrontIsClear() == true){
+    if(BeepersPresent() == true){
+        PickBeeper();
+    }
+    if (BeepersPresent() == false and FrontIsClear() == true){
+        Move();
+    }
+}
+while(BeepersPresent() == true){
+    PickBeeper();
+}
+
+FrontIsClear(); 
+BeepersPresent();
 
 }
 
@@ -22,7 +65,7 @@ void KarelProgram() {
 /// Стартовая функция
 ///
 int main() {
-    LoadWorld("worlds/collect_newspaper.w");
+    LoadWorld("worlds/CB_01.w");
     KarelProgram();
     Finish();
     return 0;
