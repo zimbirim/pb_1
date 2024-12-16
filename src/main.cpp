22019a4354a593d552214c720b3f595deeba8d4a
Евгений Козлов <j.kozlov2006ayandex.ru@gmail.com>
2024-12-16 10:00:19 +0300

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -83,7 +83,7 @@ while(FrontIsClear() == true){
 /// Стартовая функция
 ///
 int main() {
-    LoadWorld("worlds/Double_beepers_01.w");
+    LoadWorld("worlds/Double_beepers_02.w");
     KarelProgram();
     Finish();
     return 0;
